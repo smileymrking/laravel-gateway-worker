242d41addb1f5ec17421e662a19aab63da834ac6
smileymrking <smileymrking@gmail.com>
2023-07-26 11:18:07 +0800

    Add basic callbacks to message events

--- src/GatewayWorker/Push/PushEvent.php
+++ src/GatewayWorker/Push/PushEvent.php
@@ -3,12 +3,18 @@
 namespace SmileyMrKing\GatewayWorker\GatewayWorker\Push;
 
 
+use GatewayWorker\Lib\Gateway;
 use SmileyMrKing\GatewayWorker\GatewayWorker\GatewayWorkerEvents;
 
 class PushEvent extends GatewayWorkerEvents
 {
     public static function onMessage($client_id, $message)
     {
-        // Do something
+        $message = @json_decode($message, true);
+        if ($message && !empty($message['type']) && method_exists(static::class, $message['type'])) {
+            static::{$message['type']}($client_id, $message);
+        } else {
+            Gateway::sendToClient($client_id, json_encode(['type' => 'close']));
+        }
     }
 }
